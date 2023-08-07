6d6c31dbf12539c722350295b2ae7ec84fe13c03
ExstLazzaro <matteo.deangelis15@gmail.com>
2023-08-07 17:32:28 +0200

    Data Structure and base functions done

--- Pr.h
+++ Pr.h
@@ -1,6 +1,9 @@
 typedef struct ElementoBurst {
   struct ElementoBurst* prev;
   struct ElementoBurst* next;
+  int durata;
+  int occorrenza;
+
 } ElementoBurst;
 
 typedef struct ListaBurst {
@@ -12,3 +15,8 @@ typedef struct ListaBurst {
 void fetching(int listacpu[],int listaio[],int * ncpus,int * nio);
 void order(int lista[],int n);
 int cmpfunc (const void * a, const void * b);
+void List_inizializzazione(ListaBurst* list);
+ElementoBurst* List_insert(ListaBurst* head, ElementoBurst* prev, ElementoBurst* item);
+ElementoBurst* List_pushBack(ListaBurst* head, ElementoBurst* item);
+ElementoBurst* List_find(ListaBurst* head, int dato);
+void List_print(ListaBurst* head);
--- ProcessGenerator.c
+++ ProcessGenerator.c
@@ -3,11 +3,61 @@
 #include <stdio.h>
 #define MAX_BURST 100
 
+void List_inizializzazione(ListaBurst* list) {
+list->first = 0;
+list->last = 0;
+list->size = 0;
+
+}
+
+ElementoBurst* List_insert(ListaBurst* head, ElementoBurst* prev, ElementoBurst* item) {
+  if (item->next || item->prev)
+    return 0;
 
 
+  ElementoBurst* next= prev ? prev->next : head->first;
+  if (prev) {
+    item->prev=prev;
+    prev->next=item;
+  }
+  if (next) {
+    item->next=next;
+    next->prev=item;
+  }
+  if (!prev)
+    head->first=item;
+  if(!next)
+    head->last=item;
+  ++head->size;
+  return item;
+}
 
 
+ElementoBurst* List_pushBack(ListaBurst* head, ElementoBurst* item) {
+  return List_insert(head, head->last, item);
+};
 
+ElementoBurst* List_find(ListaBurst* head, int dato) {
+  // linear scanning of list
+  ElementoBurst* aux=head->first;
+  while(aux){
+    if (aux->durata==dato)
+      return aux;
+    aux=aux->next;
+  }
+  return NULL;
+}
+
+void List_print(ListaBurst* head) {
+  // linear scanning of list
+  ElementoBurst* aux=head->first;
+  while(aux){
+    printf("Burst che ha Durata %d e Occorrenza %d\n",aux->durata,aux->occorrenza);
+    aux=aux->next;
+  }
+  printf("\n---------------------------------\n");
+  
+}
 
 
 
@@ -24,7 +74,7 @@ FILE* f=fopen("Dati.txt", "r");
   size_t line_length=0;
 
   while (getline(&buffer, &line_length, f) >0){ //PRENDO LA RIGA OGNI VOLTA
-  printf("\n%d %d \n",k,y);
+  //printf("\n%d %d \n",k,y);
     int num_tokens=0;
     int duration = -1;
 
@@ -78,15 +128,70 @@ int listacpu[MAX_BURST];
 int listaio[MAX_BURST];
 
 fetching(listacpu,listaio,ncpus,nio);
-printf("\nCOSA HO PRESO----------------------\n");
+/*printf("\nCOSA HO PRESO----------------------\n");
 for(int i = 0;i< *ncpus;i++) {
 printf("CPU %d\n",listacpu[i]);
-printf("IO %d\n",listaio[i]);}
+printf("IO %d\n",listaio[i]);} */
 order(listacpu,*ncpus);
 order(listaio,*nio);
+/*
 printf("\n\nECCO ORDINATO------------------\n\n");
 for(int i = 0;i< *ncpus;i++) {
 printf("CPU %d\n",listacpu[i]);
-printf("IO %d\n",listaio[i]);}
+printf("IO %d\n",listaio[i]);
+} */
+
+ListaBurst * cpubursts = (ListaBurst *) malloc(sizeof(ListaBurst));
+ListaBurst * iobursts = (ListaBurst *) malloc(sizeof(ListaBurst));
+List_inizializzazione(cpubursts);
+List_inizializzazione(iobursts);
+
+
+
+
+
+for(int i = 0;i < *ncpus;i++ ) {
+ElementoBurst * aux = List_find(cpubursts,listacpu[i]);
+if(aux) {
+aux->occorrenza++;
+}
+else {
+ElementoBurst * new = (ElementoBurst *) malloc(sizeof(ElementoBurst));
+new->next = 0;
+new->prev = 0;
+new->durata = listacpu[i];
+new->occorrenza = 1;
+List_pushBack(cpubursts,new);
+}
+}
+
+
+for(int i = 0;i < *nio;i++ ) {
+ElementoBurst * aux = List_find(iobursts,listaio[i]);
+if(aux) {
+aux->occorrenza++;
+}
+else {
+ElementoBurst * new = (ElementoBurst *) malloc(sizeof(ElementoBurst));
+new->next = 0;
+new->prev = 0;
+new->durata = listaio[i];
+new->occorrenza = 1;
+List_pushBack(iobursts,new);
+}
+}
+
+
+
+
+printf("quanti cpu bursts %d \n",cpubursts->size);
+printf("quanti io bursts %d \n",iobursts->size);
+List_print(cpubursts);
+List_print(iobursts);
+
+
+
+
+
 return 1;
 }
